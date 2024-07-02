0f315fcaca398f482c87ad8ac1feaeaf3b73c50a
Compeador <qj6w94g0q@gmail.com>
2024-07-02 09:35:06 +0800

    feat: add restart

---
+++ run_loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do
+  # Check if it's 5:00 AM
+  if [[ $(date +"%H:%M") == "05:00" ]]; then
+    # Check if the LOCK file exists
+    if [[ -f LOCK ]]; then
+      echo "$(date +"%Y-%m-%d") Restarting server"
+      # Send CTRL+Z to the server's tmux window
+      # Use CTRL+C and remove the kill if it response to
+      tmux send-keys -t 0:2 C-z
+      kill -9 $(pgrep python)
+      sleep 1
+    fi
+  fi
+  sleep 60
+done
